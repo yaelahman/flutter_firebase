2410886d7d9e360f039e94ddde7ccc46b762a484
Manzcode <yaelahman0810@gmail.com>
2024-05-14 12:36:19 +0700

    add message

--- lib/api/firebase_api.dart
+++ lib/api/firebase_api.dart
@@ -1,119 +1,119 @@
-// import 'dart:convert';
-// import 'dart:math';
+import 'dart:convert';
+import 'dart:math';
 
-// import 'package:dio/dio.dart';
-// import 'package:firebase_messaging/firebase_messaging.dart';
-// import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-// import 'package:shared_preferences/shared_preferences.dart';
+import 'package:dio/dio.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-// class FirebaseApi {
-//   final apiUrl = "https://example.manzcode.com/api";
-//   final firebaseMessaging = FirebaseMessaging.instance;
+class FirebaseApi {
+  final apiUrl = "https://example.manzcode.com/api";
+  final firebaseMessaging = FirebaseMessaging.instance;
 
-//   final androidChannel = const AndroidNotificationChannel(
-//       'high_importance_channel', 'High Importance Notifications',
-//       description: 'This channel is used for important notifications',
-//       importance: Importance.defaultImportance);
+  final androidChannel = const AndroidNotificationChannel(
+      'high_importance_channel', 'High Importance Notifications',
+      description: 'This channel is used for important notifications',
+      importance: Importance.defaultImportance);
 
-//   final localNotifications = FlutterLocalNotificationsPlugin();
+  final localNotifications = FlutterLocalNotificationsPlugin();
 
-//   Future<void> handleBackgroundMessage(RemoteMessage message) async {
-//     print("Title: ${message.notification?.title}");
-//     print("Body: ${message.notification?.body}");
-//     print("Payload: ${message.data}");
-//     // return message;
-//   }
+  Future<void> handleBackgroundMessage(RemoteMessage message) async {
+    print("Title: ${message.notification?.title}");
+    print("Body: ${message.notification?.body}");
+    print("Payload: ${message.data}");
+    // return message;
+  }
 
-//   initNotifications() async {
-//     await firebaseMessaging.requestPermission();
-//     final FCMToken = await firebaseMessaging.getToken();
-//     print('Token: $FCMToken');
-//     storeToken(FCMToken);
-//     // FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage);
-//     initPushNotifications();
-//     initLocalNotifications();
-//     final SharedPreferences prefs = await SharedPreferences.getInstance();
-//     prefs.setString("FCMToken", FCMToken.toString());
+  initNotifications() async {
+    await firebaseMessaging.requestPermission();
+    final FCMToken = await firebaseMessaging.getToken();
+    print('Token: $FCMToken');
+    storeToken(FCMToken);
+    // FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage);
+    initPushNotifications();
+    initLocalNotifications();
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString("FCMToken", FCMToken.toString());
 
-//     return FCMToken.toString();
-//   }
+    return FCMToken.toString();
+  }
 
-//   Future<void> initPushNotifications() async {
-//     await firebaseMessaging.setForegroundNotificationPresentationOptions(
-//       alert: true,
-//       badge: true,
-//       sound: true,
-//     );
+  Future<void> initPushNotifications() async {
+    await firebaseMessaging.setForegroundNotificationPresentationOptions(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
 
-//     FirebaseMessaging.onMessage.listen((message) {
-//       final notification = message.notification;
-//       if (notification == null) return;
-//       // print("GET NOTIFICATION => " + jsonEncode(message.toMap()).toString());
+    FirebaseMessaging.onMessage.listen((message) {
+      final notification = message.notification;
+      if (notification == null) return;
+      // print("GET NOTIFICATION => " + jsonEncode(message.toMap()).toString());
 
-//       localNotifications.show(
-//         notification.hashCode,
-//         notification.title,
-//         notification.body,
-//         NotificationDetails(
-//           android: AndroidNotificationDetails(
-//               androidChannel.id, androidChannel.name,
-//               channelDescription: androidChannel.description),
-//         ),
-//         payload: jsonEncode(message.toMap()),
-//       );
-//     });
-//   }
+      localNotifications.show(
+        notification.hashCode,
+        notification.title,
+        notification.body,
+        NotificationDetails(
+          android: AndroidNotificationDetails(
+              androidChannel.id, androidChannel.name,
+              channelDescription: androidChannel.description),
+        ),
+        payload: jsonEncode(message.toMap()),
+      );
+    });
+  }
 
-//   Future<void> initLocalNotifications() async {
-//     const android = AndroidInitializationSettings('@drawable/ic_launcher');
-//     const settings = InitializationSettings(android: android);
+  Future<void> initLocalNotifications() async {
+    const android = AndroidInitializationSettings('@drawable/ic_launcher');
+    const settings = InitializationSettings(android: android);
 
-//     await localNotifications.initialize(settings);
-//   }
+    await localNotifications.initialize(settings);
+  }
 
-//   final dio = new Dio();
+  final dio = new Dio();
 
-//   storeToken(token) async {
-//     print("${apiUrl}/store-token");
-//     try {
-//       Response response = await dio
-//           .post('${apiUrl}/store-token', data: {'token': token.toString()});
-//       print("Store Token : " + response.toString());
-//     } catch (e) {
-//       print("Store Token Error " + e.toString());
-//     }
-//   }
+  storeToken(token) async {
+    print("${apiUrl}/store-token");
+    try {
+      Response response = await dio
+          .post('${apiUrl}/store-token', data: {'token': token.toString()});
+      print("Store Token : " + response.toString());
+    } catch (e) {
+      print("Store Token Error " + e.toString());
+    }
+  }
 
-//   sendMessage(token, message) async {
-//     final SharedPreferences prefs = await SharedPreferences.getInstance();
-//     final String? fromToken = prefs.getString("FCMToken");
+  sendMessage(token, message) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final String? fromToken = prefs.getString("FCMToken");
 
-//     Response response = await dio.post('${apiUrl}/send-message', data: {
-//       'token': token.toString(),
-//       'message': message.toString(),
-//       'from_token': fromToken
-//     });
+    Response response = await dio.post('${apiUrl}/send-message', data: {
+      'token': token.toString(),
+      'message': message.toString(),
+      'from_token': fromToken
+    });
 
-//     print("Store Token : " + response.toString());
-//   }
+    print("Store Token : " + response.toString());
+  }
 
-//   getChallenge(id, setChallenge) async {
-//     final SharedPreferences prefs = await SharedPreferences.getInstance();
-//     final String? fromToken = prefs.getString("FCMToken");
-//     print("ENDPOINT = ${apiUrl}/get-challenge/${id}");
+  getChallenge(id, setChallenge) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final String? fromToken = prefs.getString("FCMToken");
+    print("ENDPOINT = ${apiUrl}/get-challenge/${id}");
 
-//     Response response = await dio
-//         .post('${apiUrl}/get-challenge/${id}', data: {'token': fromToken});
+    Response response = await dio
+        .post('${apiUrl}/get-challenge/${id}', data: {'token': fromToken});
 
-//     setChallenge(response.data['data']);
-//     print("GET CHALLENGE : " + response.toString());
-//   }
+    setChallenge(response.data['data']);
+    print("GET CHALLENGE : " + response.toString());
+  }
 
-//   listToken(setTokens, token) async {
-//     final response =
-//         await dio.get('${apiUrl}/list-token?token=' + token.toString());
-//     print("LISTTOKEN => " + response.data.toString());
+  listToken(setTokens, token) async {
+    final response =
+        await dio.get('${apiUrl}/list-token?token=' + token.toString());
+    print("LISTTOKEN => " + response.data.toString());
 
-//     setTokens(response.data['data']);
-//   }
-// }
+    setTokens(response.data['data']);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_application_2/api/firebase_api.dart';
 import 'package:flutter_application_2/model/push_notification.dart';
 import 'package:overlay_support/overlay_support.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -14,12 +15,13 @@ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // Firebase.initializeApp();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  // final fcmToken = await FirebaseApi().initNotifications();
 
-  final firebaseMessaging = FirebaseMessaging.instance;
-  await firebaseMessaging.requestPermission();
-  final FCMToken = await firebaseMessaging.getToken();
-  print('Token: $FCMToken');
+  // final firebaseMessaging = FirebaseMessaging.instance;
+  // await firebaseMessaging.requestPermission();
+  // final FCMToken = await firebaseMessaging.getToken();
+  // print('Token: $FCMToken');
 
   runApp(const MyApp());
 }
@@ -53,10 +55,20 @@ class _MyHomePageState extends State<MyHomePage> {
   late final FirebaseMessaging _messaging;
   late int _totalNotifications;
   PushNotification? _notificationInfo;
+  final messageController = TextEditingController();
+  String? token = '';
 
   void registerNotification() async {
-    await Firebase.initializeApp();
+    await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform);
     _messaging = FirebaseMessaging.instance;
+
+    final FCMToken = await _messaging.getToken();
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString("FCMToken", FCMToken.toString());
+    setState(() {
+      token = FCMToken;
+    });
 
     FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
 
@@ -124,6 +136,31 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
+  void _showAlertDialog(BuildContext context) {
+    List<String> numbers = ["1", "2", "4", "5"];
+    final result = messageController.text == "3"
+        ? 'You Won'
+        : (numbers.contains(messageController.text) ? 'You Lose' : "Nothing");
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(result),
+          content: Text(messageController.text),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   void initState() {
     _totalNotifications = 0;
@@ -165,6 +202,37 @@ class _MyHomePageState extends State<MyHomePage> {
             style: TextStyle(
               color: Colors.black,
               fontSize: 20,
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(30),
+            child: Column(
+              children: [
+                const Text(
+                    "FILL NUMBER TO MAKE A CHALLENGE TO YOUR DEVICE SELECTED"),
+                TextField(
+                  controller: messageController,
+                  keyboardType: const TextInputType.numberWithOptions(
+                      decimal: false), // Allow decimal numbers
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.digitsOnly // Allow only digits
+                  ],
+                ),
+                TextButton(
+                  onPressed: (() {
+                    FirebaseApi().sendMessage(token, messageController.text);
+                    _showAlertDialog(context);
+                  }),
+                  child: Container(
+                    color: Colors.blue,
+                    padding: EdgeInsets.all(10),
+                    child: const Text(
+                      "SEND",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
           SizedBox(height: 16.0),
